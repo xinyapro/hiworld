15d2bd13fa8bdcbfe8d83b0cd01bc61012a26fcc
xinyapro <zxy-y@qq.com>
2020-05-05 23:27:55 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Description:
+ *
+ * @author zhuxinya
+ * @date 2020/5/5 23:27
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World!");
+    }
+}
